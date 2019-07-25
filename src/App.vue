40db89895b23389832bec5c5b8b936e6165a0ae4
Lucas Sonsin Lima <lucassonsin.lima@gmail.com>
2019-07-25 15:22:31 -0300

    Core - Create Base Structure with vue-cli

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Yey Vue Playground"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Cats from './components/Cats.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    Cats
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Cats.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class='c-cats'>
+    <article class="c-cats__item" v-for="info in catInfos" v-bind:key="info.id">
+      <div>{{ info.text }}</div>
+    </article>
+  </section>
+</template>
+<script>
+export default {
+  name: 'Cats',
+  methods: {
+    fetchCats(){
+      const api = `https://cat-fact.herokuapp.com/facts`
+      return fetch(api)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.c-cats {
+  border: 1px solid red;
+  padding: 15px;
+}
+
+.c-cats__item {
+  border: 1px solid yellow;
+  margin: 10px 0;
+  padding: 10px;
+}
+</style>
