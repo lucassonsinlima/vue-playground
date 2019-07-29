820ab64c294d89358eac9b093a075555f54e147e
Lucas Sonsin Lima <lucassonsin.lima@gmail.com>
2019-07-29 18:33:21 -0300

    Core - Router with Async Routes Example

---
+++ src/components/async/AsyncExample.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="c-async">
+    <h1>Async Component</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/pages/Async.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="async">
+    <AsyncExample/>
+  </div>
+</template>
+
+<script>
+import AsyncExample from '@/components/async/AsyncExample.vue'
+
+export default {
+  name: 'Async',
+  components: {
+    AsyncExample
+  }
+}
+</script>
