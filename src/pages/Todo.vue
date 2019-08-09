e03bad35ef5b9358af2fd7b8358c4a9dada4becb
Lucas Sonsin Lima <lucassonsin.lima@gmail.com>
2019-08-09 15:29:14 -0300

    Create Todoist Grid and GridItem components

--- src/pages/Todo.vue
+++ src/pages/Todo.vue
@@ -1,17 +1,59 @@
 <template>
-  <div>
-    <h1>zz</h1>
+  <div class="todo">
+    <h1>Todoist</h1>
+    <TodoTasksGrid :tasks="tasks" />
   </div>
 </template>
 
 <script>
-export default {
+import TodoTasksGrid from "@/components/todo/TodoTasksGrid.vue"
 
+export default {
+  components: {
+    TodoTasksGrid
+  },
+  data() {
+    return {
+      tasks: [
+        { name: 'Cook Potatoes', pending: false },
+        { name: 'Foo', pending: false },
+        { name: 'Making mistakes', pending: false },
+        { name: 'Doe', pending: false },
+      ]
+    }
+  },
 }
 </script>
 
 <style scoped lang="scss">
-  body {
-    background: purple;
+
+$green: #38cf9a;
+$blue: #00386B;
+
+%bg {
+  &--blue {
+    background: $blue;
   }
+}
+
+%h1 {
+  font-size: 40px;
+}
+
+h1 {
+  @extend %h1;
+}
+
+.todo {
+  @extend %bg--blue;
+
+  color: #fff;
+  height: 100vh;
+  display: flex;
+  flex-flow: column;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
 </style>
